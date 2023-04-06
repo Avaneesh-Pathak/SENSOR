17cc297bfd3266f04c6ed965767199423025f74b
hrisikeshOfc <hrishikesh@ineuron.ai>
2023-04-06 14:02:49 +0530

    app updated

--- app.py
+++ app.py
@@ -26,9 +26,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-templates = Jinja2Templates(directory="templates")
- 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 
